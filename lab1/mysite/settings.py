9a7863cc54fc8f0b1316adc3e077295df0139de9
MichalKohnke <72730788+MichalKohnke@users.noreply.github.com>
2022-01-30 22:21:24 +0100

    Update settings.py

--- lab1/mysite/settings.py
+++ lab1/mysite/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'db',
+        'USER': 'user01',
+        'PASSWORD': 'jU73G8382+',
+        'HOST': 'mysql',
+        'PORT': '3306',
+    },
 }
 
 
